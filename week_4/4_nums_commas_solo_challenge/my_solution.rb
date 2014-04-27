a1ae4e83659fcf0c31753577e595c8c73782c291
Brennon Williams <brennonw@gmail.com>
2014-04-27 00:06:49 -0400

    solved week 4 solo challenge

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -7,18 +7,54 @@
 
 # 1. Pseudocode
 
-# What is the input?
+# What is the input? 
+
+# An integer
+
 # What is the output? (i.e. What should the code return?)
+
+# A string that is appropriately seperated by commmas
+
 # What are the steps needed to solve the problem?
+
+# Define method
+# Get integer number of characters
+# convert integer to string
+# remove last character off integer (now string)
+# add that character to a new string
+# if there have been three characters since the last comma and-
+# -there are still more characters to add then draw a comma
+# return the new string
 
 
 # 2. Initial Solution
 
+def commaInt number
+	new_string = ""
+	number_string = number.to_s
+	char_counter = 0
 
+	while number_string.size > 0
+		new_string = number_string.chars.last + new_string
+		number_string = number_string.chomp(number_string.chars.last)
+		char_counter = char_counter + 1
+		if char_counter%3 == 0 && number_string.size > 0
+			new_string = "," + new_string
+		end
+	end
+	new_string
+end
 
 
 # 3. Refactored Solution
 
-
+# I went through the D.R.Y. standards and my code appears to be alright. The names 
+# make sense (hopefully) and its a pretty concise program. I'm not sure how to
+# refactor it
 
 # 4. Reflection 
+
+# I love how easy it is to combine strings in Ruby, its just like adding two numbers together.
+# Originally I used a comma counter and added it to the string size and modded it by three to determine 
+# if a comma should be added. This wasn't bad per se, but it was ugly and unnecessary so I decided to use
+# a simpler character counter instead (refactoring on the fly!).
